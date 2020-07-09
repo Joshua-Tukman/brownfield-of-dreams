9d93221843f8221e463dd37483891336de677a3c
Josh Tukman <josh.tukman@gmail.com>
2020-07-08 22:39:41 -0600

    Add methods to user_video model to create bookmarked videos and send to controller

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,4 +1,19 @@
 class UserVideo < ApplicationRecord
   belongs_to :video
   belongs_to :user
+
+  def self.bookmarked_videos(user)
+    order_bookmarks(collect_bookmarks(user))
+  end
+
+  def self.collect_bookmarks(user)
+    user.videos.select(:title, :id, :position, 'tutorials.title AS tutorial_title', 'tutorials.id AS tutorial_id').joins(:tutorial).order(:tutorial_id, :position)
+  end 
+
+  def self.order_bookmarks(bookmarks)
+    bookmarks.group_by(&:tutorial_title)
+  end
+
 end
+
+
